047d15f76dbee8c373a402f2964cc54335419852
nadjibnano <nadjimess@gmail.come>
2021-02-23 14:39:24 +0100

    split_pdf_named is modifed

--- split_pdf_named.py
+++ split_pdf_named.py
@@ -8,6 +8,8 @@ if len(args) > 1:
     pdf_file_path = args[1]
     print("The File Name Is:" , pdf_file_path)
 
+#os.makedirs(str(pdf_file_path))
+
 '''with open(pdf_file_path, "rb") as filehandle:
     pdf = PdfFileReader(filehandle)
     info = pdf.getDocumentInfo()
@@ -25,11 +27,11 @@ if len(args) > 1:
 file_base_name = pdf_file_path.replace('.pdf', '')
 output_folder_path = os.path.join(os.getcwd(), 'Output')
 
-wb = load_workbook('momber_com_scien.xlsx')
+wb = load_workbook('Class_momb.xlsx')
 sheet = wb['Feuil1']
 max_row = sheet.max_row
 list_nom=[]
-for i in range(2, 17):
+for i in range(1, 29):
     cell_name = sheet.cell(row=i, column=1)
     nam_lastnam=str(cell_name.value)
     list_nom.append(nam_lastnam)
@@ -41,7 +43,7 @@ for page_num in range(pdf.numPages):
     pdfWriter = PdfFileWriter()
     pdfWriter.addPage(pdf.getPage(page_num))
     
-    with open(os.path.join(output_folder_path, '{0}{1}.pdf'.format(list_nom[page_num+1], page_num+1)), 'wb') as f:
+    with open(os.path.join(output_folder_path, '{0}.pdf'.format(list_nom[page_num+1] )), 'wb') as f:
         pdfWriter.write(f)
-        print("The Splite Is Done", list_nom[page_num+1])
+        print("The Splite Is Done", page_num+1, list_nom[page_num+1])
         f.close()
